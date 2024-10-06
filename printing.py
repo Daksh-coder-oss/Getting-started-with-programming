db3fe52cc83f16f73716268a8b56e6f979ccd1c7
Daksh-coder-oss <vv0039190@gmail.com>
2024-10-06 10:22:46 +0100

    Add files via upload

---
+++ printing.py
@@ -0,0 +1,10 @@
+print("hello")
+
+n=7
+
+print(n)
+
+print("hello",n)
+
+
+print("welcome","*")     
---
+++ variables.py
@@ -0,0 +1,5 @@
+name ="Daksh"   
+marks="95"
+print(name,"scored",marks)
+name=input("Please enter your name")
+print("Welcome",name)
